756088b88a8cacf9f08728dc0233b9d832edb219
Steffi <steffi.tan.2018@economics.smu.edu.sg>
2020-09-06 19:18:38 +0800

    ui update

--- frontend/static/js/functions.js
+++ frontend/static/js/functions.js
@@ -1,33 +1,33 @@
 async function getAllIndustries() {
-    var timNemURL = "http://timnem.tk/api/industries";
+    var timNemURL = "http://app.timnem.tk/api/industries";
     var industriesJSON = await fetch(timNemURL, {method: "GET"});
     var data = await industriesJSON.json();
     return data;
 }
 
 async function getAllCompanies() {
-    var timNemURL = "http://timnem.tk/api/companies";
+    var timNemURL = "http://app.timnem.tk/api/companies";
     var companiesJSON = await fetch(timNemURL, {method: "GET"});
     var data = await companiesJSON.json();
     return data;
 }
 
 async function getCompanyBySymbol(symbol) {
-    var timNemURL = `http://timnem.tk/api/companies/symbol/${symbol}`;
+    var timNemURL = `http://app.timnem.tk/api/companies/symbol/${symbol}`;
     var companyJSON = await fetch(timNemURL, {method: "GET"});
     var data = await companyJSON.json();
     return data;
 }
 
 async function searchCompaniesBySymbol(symbol) {
-    var timNemURL = `http://timnem.tk/api/companies/search/symbol/${symbol}`;
+    var timNemURL = `http://app.timnem.tk/api/companies/search/symbol/${symbol}`;
     var companiesJSON = await fetch(timNemURL, {method: "GET"});
     var data = await companiesJSON.json();
     return data;
 }
 
 async function getCompaniesByIndustry(industry) {
-    var timNemURL = `http://timnem.tk/api/companies/industry/${industry}`;
+    var timNemURL = `http://app.timnem.tk/api/companies/industry/${industry}`;
     var companiesJSON = await fetch(timNemURL, {method: "GET"});
     var data = await companiesJSON.json();
     return data;
